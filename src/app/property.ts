edfa724efb74261578fa37f7451dbe7d63ca6721
Kun Zou <kun.zou@cwbnationalleasing.com>
2019-12-19 15:41:00 -0600

    in progress

--- src/app/property.ts
+++ src/app/property.ts
@@ -2,5 +2,33 @@ export interface Property {
     id: string;
     name: string;
     address: string;
-    imgUrl: string;
+    primaryImgUrl: string;
+    imgUrls: string[];
+    houseType: string;
+    numberOfRooms: number;
+    numberOfWashrooms: number;
+    area: number;    
+    askingPrice: number;
+    finalPrice: number;
+    onMarketSince: Date;
+    dealDate: Date;
 }
+
+enum HouseType {
+    DETACHED_HOUSE = "Detached House",
+    TOWN_HOUSE = "Town House",
+    CONDO = "Condo",
+}
+
+
+
+// private String primaryImgUrl;
+// private List<String> imgUrls;
+// private HouseType houseType;
+// private BigDecimal askingForPrice;
+// private BigDecimal finalPrice;
+// private LocalDate listingDate;
+// private LocalDate dealDate;
+// private Integer numberOfRooms;
+// private Integer numberOfWashRooms;
+// private Double area;
