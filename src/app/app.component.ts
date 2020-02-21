735326c87434e2f93312ec71d943fb0f2e72cac1
kun <kun.zou@gmail.com>
2020-02-20 23:17:49 -0600

    add links to footer routeLink scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from './auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = '刘聃，福居温尼伯';
   constructor(
     public translateService: TranslateService,
     public auth: AuthService,
+    private router: Router
   ) {
     auth.handleAuthentication();
     translateService.addLangs(['en', 'zh']);
     translateService.setDefaultLang('zh');
     translateService.use('zh');
   }
+  
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+        if (!(evt instanceof NavigationEnd)) {
+            return;
+        }
+        window.scrollTo(0, 0)
+    });
+  }  
 
   switchLanguage(): void {
     if(this.translateService.currentLang === 'en') {
